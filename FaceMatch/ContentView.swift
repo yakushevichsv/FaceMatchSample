d3d5ae58e0e1a9b586f819236a9ff56648499f3c
Siarhei Yakushevich <yakushevichsv@hotmail.com>
2021-10-09 12:47:08 +0300

    Initial Commit

---
+++ FaceMatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FaceMatch
+//
+//  Created by Siarhei Yakushevich on 9.10.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FaceMatch/FaceMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceMatchApp.swift
+//  FaceMatch
+//
+//  Created by Siarhei Yakushevich on 9.10.21.
+//
+
+import SwiftUI
+
+@main
+struct FaceMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
